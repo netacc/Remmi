50ae037e778d4aee27af03bd73c27e57ed5ed42e
netacc <spywareses@gmail.com>
2018-07-25 11:31:36 +0300

    news

--- platforms/browser/www/js/routes.js
+++ platforms/browser/www/js/routes.js
@@ -1,50 +1,58 @@
 var routes = [
-  {
-      path: '/',
-      url: './index.html',
-  },
-  {
-    path: '(/accordion/)',
-    url: './pages/accordion.html',
-  }, 
-  {
-    path: '(/about/)',
-    url: './pages/about.html',
-  },
-  {
-    path: '(/contact/)',
-    url: './pages/contact.html',
-  },
-  {
-    path: '(/ivan-tasks/)',
-    url: './pages/ivan-tasks.html',
-  },
-  {
-      path: '(/ivan-task-1/)',
-      url: './pages/ivan-task-1.html',
-  },
-  {
-      path: '(/ivan-task-2/)',
-      url: './pages/ivan-task-2.html',
-  },
+    {
+        path: '/',
+        url: './index.html',
+    },
+    {
+        path: '(/accordion/)',
+        url: './pages/accordion.html',
+    },
+    {
+        path: '(/about/)',
+        url: './pages/about.html',
+    },
+    {
+        path: '(/contact/)',
+        url: './pages/contact.html',
+    },
+    {
+        path: '(/ivan-tasks/)',
+        url: './pages/ivan-tasks.html',
+    },
+    {
+        path: '(/ivan-task-1/)',
+        url: './pages/ivan-task-1.html',
+    },
+    {
+        path: '(/ivan-task-2/)',
+        url: './pages/ivan-task-2.html',
+    },
 
-  {
-      path: '(/petr-tasks/)',
-      url: './pages/petr-tasks.html',
-  },
+    {
+        path: '(/petr-tasks/)',
+        url: './pages/petr-tasks.html',
+    },
 
-  {
-      path: '(/petr-task-1/)',
-      url: './pages/petr-task-1.html',
-  },
+    {
+        path: '(/petr-task-1/)',
+        url: './pages/petr-task-1.html',
+    },
 
-  {
-      path: '(/contact-list/)',
-      url: './pages/contact-list.html',
-  },
+    {
+        path: '(/petr-task-2/)',
+        url: './pages/petr-task-2.html',
+    },
+    {
+        path: '(/my-task/)',
+        url: './pages/my-task.html',
+    },
+    {
+        path: '(/contact-list/)',
+        url: './pages/contact-list.html',
+    },
     // Default route (404 page). MUST BE THE LAST
-  {
-    path: '(.*)',
-    url: './pages/404.html',
-  },  
+    {
+        path: '(.*)',
+        url: './pages/404.html',
+    },
 ];
--- www/js/app.js
+++ www/js/app.js
@@ -1,25 +1,83 @@
-var $ = Dom7;
+var $$ = Dom7;
 var app = new Framework7({
-  // App root element
-  root: '#app',
-  // App Name
-  name: 'Remmi',
-  // App id
-  id: 'ru.remmi',
-  touch: {
+    // App root element
+    root: '#app',
+    // App Name
+    name: 'Remmi',
+    // App id
+    id: 'ru.remmi',
+    touch: {
       materialRipple: false
-  },
-  view: {
+    },
+    // Enable templates auto precompilation
+    precompileTemplates: true,
+
+    // Enabled rendering pages using Template7
+    template7Pages: true,
+    view: {
       pushState: true,
       stackPages:true,
-  },
-  data: function () {
-    return {
-      user: {
-        firstName: 'John',
-        lastName: 'Doe',
-      },
-    };
-  },
-  routes: routes,
+    },
+    routes: routes,
+
+    on: {
+        // each object key means same name event handler
+        pageInit: function (page) {
+
+        },
+        popupOpen: function (popup) {
+            // do something on popup open
+        },
+    },
+
 });
+;
+
+
+app.api = ({
+    createUser: function(){
+        app.request.postJSON('http://192.168.88.177:8989/rest/personservice/person/post/',
+            JSON.parse('{"person":[{"name":"Test Person", "age":100, "id":1}]}'),
+            function (data) {
+                console.log(data)
+            });
+    },
+    updateUsers: function(){
+        app.request.get('http://192.168.88.177:8989/rest/personservice/person/get/', 0,
+            function (data, status, xhr) {
+                console.log(data)
+                console.log(status)
+                var context = JSON.parse(data);
+                var template = $$('script#template').html();
+                var compiledTemplate = Template7.compile(template);
+                var html = compiledTemplate(context);
+
+                $$('.main-content').append(html);
+            },
+            function (xhr, status){
+                console.log(status)
+            })
+    },
+});
+
+;
+
+$$('.notification-single').on('click', function () {
+    app.api.createUser();
+    app.api.updateUsers();
+    app.dialog.alert('User create');
+});
+app.request.get('http://192.168.88.177:8989/rest/personservice/person/get/', 0,
+    function (data, status, xhr) {
+        console.log(data)
+        console.log(status)
+        var context = JSON.parse(data);
+        var template = $$('script#template').html();
+        var compiledTemplate = Template7.compile(template);
+        var html = compiledTemplate(context);
+
+        $$('.main-content').append(html);
+    },
+    function (xhr, status){
+        console.log(status)
+    })
